57c67d8c973148c48c05e140d0f82ff65fddb70f
George <george.suporte@gmail.com>
2020-11-13 01:05:04 -0300

    [PROJETO] Commit inicial.

---
+++ backend/src/test/java/com/cadastra/cep/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cadastra.cep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
