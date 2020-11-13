8abf2a56ec8f420013e0b5e767c71de3f5a745fa
George <george.suporte@gmail.com>
2020-11-13 02:55:23 -0300

    [UPDATE] Atualizado nome da Entity.

---
+++ backend/src/main/java/com/cadastra/cep/entity/ZipCodeEntity.java
@@ -0,0 +1,28 @@
+package com.cadastra.cep.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Data
+@Entity(name = "CEP")
+public class ZipCodeEntity {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+
+    private String cep;
+    private String logradouro;
+    private String complemento;
+    private String bairro;
+    private String localidade;
+    private String uf;
+    private String ibge;
+    private String gia;
+    private String ddd;
+    private String siafi;
+}
